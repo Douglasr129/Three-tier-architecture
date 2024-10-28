81bdc524806c3921e69bc6a84f0ddadfd59ef153
Douglas Rodrigues <Douglas.R129@outlook.com>
2024-10-28 18:47:52 -0300

    Configurando a camada Data

---
+++ src/DevIO.Data/Mappings/FornecedorMapping.cs
@@ -0,0 +1,30 @@
+﻿
+using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DevIO.Data.Mappings
+{
+    public class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
+    {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+            builder.Property(x => x.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            builder.HasOne(p => p.Endereco)
+                .WithOne(e => e.Fornecedor);
+
+            builder.HasMany(p => p.Produtos)
+                .WithOne(f => f.Fornecedor)
+                .HasForeignKey(e => e.FornecedorId);
+
+            builder.ToTable("Fornecedores");
+        }
+    }
+}
